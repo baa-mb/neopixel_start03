e241dba4d002f5d514938890a9e793b9ea3159fa
baa <34665120+baa-mb@users.noreply.github.com>
2021-03-12 00:02:54 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,38 @@
 function init_matrix () {
-    matrix_breite = 16
-    pixel_anzahl = 64
-    strip = neopixel.create(DigitalPin.P0, pixel_anzahl, NeoPixelMode.RGB)
+    not_aus = 0
+    pixel_anzahl = 16
+    matrix_breite = 4
+    strip = neopixel.create(DigitalPin.P0, pixel_anzahl, NeoPixelMode.RGB_RGB)
     strip.setMatrixWidth(matrix_breite)
-    strip.setBrightness(20)
+    strip.setBrightness(150)
 }
 input.onButtonPressed(Button.A, function () {
-    strip.showRainbow(1, 360)
+    not_aus = 0
 })
+function pixel_ein (x: number, y: number) {
+    strip.setMatrixColor(y, x, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
+    basic.pause(2000)
+    strip.clear()
+    basic.pause(2000)
+}
 input.onButtonPressed(Button.B, function () {
     strip.clear()
 })
+let stromverbrauch = 0
 let strip: neopixel.Strip = null
-let pixel_anzahl = 0
 let matrix_breite = 0
+let pixel_anzahl = 0
+let not_aus = 0
 init_matrix()
+// serial.writeValue("x", stromverbrauch)
 basic.forever(function () {
-    for (let y = 0; y <= 3; y++) {
-        for (let x = 0; x <= matrix_breite; x++) {
-            strip.setMatrixColor(x, y, neopixel.colors(NeoPixelColors.Red))
-            strip.show()
-            basic.pause(20)
-            strip.setMatrixColor(x, y, neopixel.colors(NeoPixelColors.Black))
-            strip.show()
-        }
+    stromverbrauch = strip.power()
+    if (stromverbrauch > 45) {
+        not_aus = 1
+        strip.clear()
     }
-    serial.writeValue("x", strip.power())
+    if (not_aus == 0) {
+        pixel_ein(0, 1)
+    }
 })
